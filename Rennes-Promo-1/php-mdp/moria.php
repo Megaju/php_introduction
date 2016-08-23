74acc2c37b1c7e18a83c739ecf0a81ba8d2e7f32
Megaju <julien_malle@outlook.fr>
2016-08-23 11:31:10 +0200

    exo password ok + style Moria ! ! !

--- Rennes-Promo-1/php-mdp/moria.php
+++ Rennes-Promo-1/php-mdp/moria.php
@@ -6,8 +6,33 @@
 </head>
 <body>
     <?php 
-        if ($_POST["password"] == $_SESSION["password-of-the-door"]) {
+        if ($_POST['mdp'] == 'mellon') {
             ?>
+            <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        
+        body {
+            padding: 100px 0;
+            text-align: center;
+            background-color: #000;
+            background-image: url(bg-moria.jpg);
+            background-size: cover;
+            background-position: center center;
+            background-repeat: no-repeat;
+            color: #fff;
+        }
+        
+        h1, p, i, form, input {
+            color: #fff;
+            margin: 10px 0;
+            padding: 10px;
+            background-color: rgba(0, 0, 0, 0.8);
+        }
+    </style>
+            
             <!-- contenue du site -->
             <h1>Mine de la Moria !</h1>
             <p>Bravo tu es rentrer dans les mines de la Moria !</p>
@@ -19,5 +44,6 @@
             echo ('<p>Désolé ce n\'est pas le bon mot de passe. Va lire ou cherche sur internet noob !</p>');
         }
     ?>
+    
 </body>
 </html>
--- Rennes-Promo-1/php-mdp/the_door.php
+++ Rennes-Promo-1/php-mdp/the_door.php
@@ -1,4 +1,3 @@
-<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,15 +5,37 @@
     <title>Page protégée par MDP avec PHP</title>
 </head>
 <body>
-    <?php $_SESSION["password-of-the-door"] = "mellon" ?>
-    
     <p>Trouve le mot de passe pour rentrer dans les mines de la Moria.</p>
     <i>Comme ça on verra si t'es du genre à voir seulement les films où lire les livres, muhahahaha !!!!!</i>
     <form action="moria.php" method="POST">
-        <input type="password" name="password">
+        <input type="password" name="mdp">
         <input type="submit" value="Valider">
     </form>
     
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        
+        body {
+            padding: 100px 0;
+            text-align: center;
+            background-color: #000;
+            background-image: url(bg-gate.jpg);
+            background-size: cover;
+            background-position: center center;
+            background-repeat: no-repeat;
+            color: #fff;
+        }
+        
+        h1, p, i, form, input {
+            color: #fff;
+            margin: 10px 0;
+            padding: 10px;
+            background-color: rgba(0, 0, 0, 0.8);
+        }
+    </style>
     
 </body>
 </html>
