8fbb257a6919e51e274fd7c935267213436e01d6
Megaju <julien_malle@outlook.fr>
2016-08-24 10:11:07 +0200

    bdd modifie - bug resolve + champ prenom + heure

--- Rennes-Promo-1/minichat/deathnote.php
+++ Rennes-Promo-1/minichat/deathnote.php
@@ -39,22 +39,35 @@
             <form action="post.php" method="post">
                 <label for="name">Nom</label>
                 <input type="text" name="nom" id="nom">
+                <label for="name">Prénom</label>
+                <input type="text" name="prenom" id="prenom">
                 <label for="message">Cause de la mort</label>
                 <textarea name="message" id="message" cols="30" rows="10">Crise cardiaque.</textarea>
-                <label for="date">Date du décès</label>
-                <input type="date" name="date" id="date">
+                <label for="dateofdeath">Date du décès</label>
+                <input type="datetime-local" name="dateofdeath" id="dateofdeath">
                 <input type="submit">
             </form>
             
             <div id="deathnote-style">
             <?php
             // Récupération des 50 derniers messages
-            $reponse = $bdd->query('SELECT nom, message, date FROM deathnote ORDER BY ID DESC LIMIT 0, 50');
+            $reponse = $bdd->query('SELECT nom, prenom, message, dateofdeath FROM deathnote ORDER BY ID DESC LIMIT 0, 50');
 
             // /!\IMPORTANT/!\ Affichage de chaque message (données protégées par htmlspecialchars) /!\IMPORTANT/!\
             while ($donnees = $reponse->fetch())
             {
-                echo '<p class="death"><strong>Nom :</strong> <span class="name">' . htmlspecialchars($donnees['nom']) . ' </span><strong>Cause :</strong> ' . htmlspecialchars($donnees['message']) . ' <strong>Date:</strong> ' . htmlspecialchars($donnees['date']) . '</p>';
+                echo '<p class="death">
+                <strong>Nom :</strong> <span class="name">' . 
+                htmlspecialchars($donnees['nom']) . 
+                ' </span>' . 
+                '<strong>Prénom :</strong><span class="name">' .
+                htmlspecialchars($donnees['prenom']) .
+                ' </span>' .
+                '<strong>Cause :</strong> ' . 
+                htmlspecialchars($donnees['message']) . 
+                ' le ' . 
+                htmlspecialchars($donnees['dateofdeath']) . 
+                '</p>';
             }
 
             $reponse->closeCursor();
--- Rennes-Promo-1/minichat/post.php
+++ Rennes-Promo-1/minichat/post.php
@@ -12,8 +12,8 @@
             
 
 // Insertion du message à l'aide d'une requête préparée
-$req = $bdd->prepare('INSERT INTO deathnote (nom, message, date) VALUES(?, ?, ?)');
-$req->execute(array($_POST['nom'], $_POST['message'], $_POST['date']));
+$req = $bdd->prepare('INSERT INTO deathnote (nom, prenom, message, dateofdeath) VALUES(?, ?, ?, ?)');
+$req->execute(array($_POST['nom'], $_POST['prenom'], $_POST['message'], $_POST['dateofdeath']));
 
 // Redirection du visiteur vers la page du minichat
 header('Location: deathnote.php');
