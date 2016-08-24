a9d9703529dafc6a685dab2791546924ab3cefc8
Megaju <julien_malle@outlook.fr>
2016-08-24 19:47:36 +0200

    on peut mnt poster des news en mode admin

--- Rennes-Promo-1/minichat/write_news.php
+++ Rennes-Promo-1/minichat/write_news.php
@@ -1,9 +1,20 @@
 <?php 
-        if ($_POST['mdp'] == 'J2kyu$42#X') {
+        if ($_POST['mdp'] == 'lol') {
             ?>
-            <!-- contenue du site -->
-            <p>Bientôt ici on pourra écrire une news c'est super non ?</p>
-            <?php    
+            <?php include('header.php'); ?>
+            <form action="post_news.php" method="post">
+                <!-- titre de la news -->
+                <label for="title">Titre</label>
+                <input type="text" name="title" id="title">
+                <!-- message -->
+                <label for="content">Message</label>
+                <textarea type="text" name="content" id="content" rows="10" cols="30"></textarea>
+                <!-- date -->
+                <input type="hidden" name="date_creation" id="date_creation" value="<?php echo date(" Y-m-d H:i:s "); ?>">
+                <!-- envoyer -->
+                <input type="submit">
+            </form>
+    <?php    
         } else {
             echo ('<p>Désolé ce n\'est pas le bon mot de passe ton chemin muhahahaha !</p>');
         }
