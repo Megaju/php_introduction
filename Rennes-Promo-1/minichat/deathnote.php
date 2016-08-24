167ff73314f5d1dba6a7d2340f5e781ea4174eb1
Megaju <julien_malle@outlook.fr>
2016-08-24 09:53:41 +0200

    2 bdd local, 1 un en comm

--- Rennes-Promo-1/minichat/deathnote.php
+++ Rennes-Promo-1/minichat/deathnote.php
@@ -15,7 +15,8 @@
             <?php
             try
             {
-                $bdd = new PDO('mysql:host=localhost;dbname=deathnotebdd;charset=utf8', 'root', '');
+                $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', 'facesimplon');
+                //$bdd = new PDO('mysql:host=localhost;dbname=deathnotebdd;charset=utf8', 'root', '');
             }
             catch(Exception $e)
             {
